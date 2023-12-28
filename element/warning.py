03af43f56b37fb596764b629d44af0ba0c4bb4d0
Pavel Tuktarov <redmain2@gmail.com>
2023-12-29 00:03:10 +0800

    Create WarningDialog class

---
+++ element/warning.py
@@ -0,0 +1,19 @@
+from PySide6.QtWidgets import QDialog, QDialogButtonBox, QVBoxLayout, QLabel
+
+class WarningDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Error!")
+
+        QBtn = QDialogButtonBox.StandardButton.Ok
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        message = QLabel("Wrong ID, IP, KEY or Version")
+        self.layout.addWidget(message)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
